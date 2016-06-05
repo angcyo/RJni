d4eb55f2ceeb7cfbb3ff1b2edf4394859ecce9b6
angcyo <angcyo@126.com>
2016-06-05 23:04:56 +0800

    add jni module

--- app/src/main/java/com/angcyo/rjni/MainActivity.java
+++ app/src/main/java/com/angcyo/rjni/MainActivity.java
@@ -5,9 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import com.jni.NdkHello;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.e("angcyo jni", NdkHello.getHello());
                 Snackbar.make(view, "Replace with your test own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
--- jnitest/src/main/java/com/jni/NdkHello.java
+++ jnitest/src/main/java/com/jni/NdkHello.java
@@ -4,6 +4,11 @@ package com.jni;
  * Created by angcyo on 2016-06-05 00:03.
  */
 public class NdkHello {
+
+    static {
+        System.loadLibrary("HelloJni");
+    }
+
     //返回字符串
     public static native String getHello();
 }
