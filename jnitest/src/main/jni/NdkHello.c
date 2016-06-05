eecfc2e2b0c34a603e2359b41c9ed3fc786158fb
angcyo <angcyo@126.com>
2016-06-05 23:19:18 +0800

    add jni module

--- jnitest/src/main/jni/NdkHello.c
+++ jnitest/src/main/jni/NdkHello.c
@@ -1,9 +1,11 @@
+#include <jni.h>
 #include <android/log.h>
+#include "com_jni_NdkHello.h"
 
 #define TAG "angcyo"
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
 
-JNIEXPORT jstringJNICALL Java_com_jni_NdkHello_getHello
+JNIEXPORT jstring JNICALL Java_com_jni_NdkHello_getHello
         (JNIEnv *env, jclass jcs){
     jstring hello = "log in jni, from c++. 2";
     LOGD("log in jni, from c++. NdkHello");
