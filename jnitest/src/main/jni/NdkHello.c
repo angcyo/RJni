7eaed2c08403cd21af7598c83790f991ff253bd5
angcyo <angcyo@126.com>
2016-06-12 18:16:57 +0800

    add ldLibs

--- jnitest/src/main/jni/NdkHello.c
+++ jnitest/src/main/jni/NdkHello.c
@@ -5,8 +5,7 @@
 #define TAG "angcyo"
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
 
-JNIEXPORT jstring JNICALL Java_com_jni_NdkHello_getHello
-        (JNIEnv *env, jclass jcs){
+JNIEXPORT jstring JNICALL Java_com_jni_NdkHello_getHello(JNIEnv *env, jclass jcs){
     jstring hello = "log in jni, from c++. 2";
     LOGD("log in jni, from c++. NdkHello");
     return (*env)->NewStringUTF(env, "from jni ...");
